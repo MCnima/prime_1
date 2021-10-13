000dc0a79491fced31a854e983097a4005b0c29d
MCnima <www.nimahabibi1382@gmail.com>
2021-10-13 14:44:22 +0330

    Last check!

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import sys
 
-Number = int(sys.argv[1])
-factor_counter = 0
-for factor in range(1, Number+1):
-    if Number % factor == 0:
-        factor_counter += 1
-if factor_counter == 2:
-    print('Number is prime')
-else:
-    print('Number is NOT prime')
+
+def main():
+    number = int(sys.argv[1])
+    factor_counter = 0
+    for factor in range(1, number+1):
+        if number % factor == 0:
+            factor_counter += 1
+    if factor_counter == 2:
+        print('Number is prime')
+    else:
+        print('Number is NOT prime')
+
+
+if __name__ == '__main__':
+    main()
